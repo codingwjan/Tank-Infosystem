130368b7844bf7175c6f71af62e7c6ed37afe1f1
Stevebuster <114467890+Stevebuster@users.noreply.github.com>
2022-09-27 09:14:26 +0200

    Add files via upload

---
+++ infotainment.java
@@ -0,0 +1,68 @@
+import java.net.URISyntaxException;
+import java.io.IOException;
+import java.util.Scanner;
+import java.awt.Desktop;
+import java.net.URI;
+
+public class infotainment {
+    public static void main(String[] args) throws IOException, URISyntaxException{
+        double tank;
+        double verbrauch;
+        double strecke;
+        double ppl;
+        
+        String tanke = "https://waa.ai/f6lY";
+        
+        double tankrest;
+        double fahrtkosten;
+
+        Scanner scan = new Scanner(System.in);
+                        System.out.println("Tankinhalt");
+                        tank = scan.nextDouble();
+
+                        System.out.println("Verbrauch pro 100km");
+                        verbrauch = scan.nextDouble();
+                        
+                        System.out.println("Gefahrene Strecke");
+                        strecke = scan.nextDouble();
+
+                        System.out.println("preis pro liter");
+                        ppl = scan.nextDouble();
+
+                        tankrest = (tank - verbrauch * strecke / 100);
+                        fahrtkosten = (tank - tankrest * ppl);
+                        
+                        if (tank < 0) {
+                            System.out.println("tank unrealistisch");
+                            System.exit(0);
+                        }
+                        if (verbrauch < 0) {
+                            System.out.println("verbrauch unrealistisch");
+                            System.exit(0);
+                        }
+                        if (strecke < 0) {
+                            System.out.println("strecke unrealistisch");
+                            System.exit(0);
+                        }
+                        if (ppl < 0) {
+                            System.out.println("preis pro liter unrealistisch");
+                            System.exit(0);
+                        }
+
+                        if (tankrest < 0) {
+                            System.out.println("geh tanken ");
+                            Desktop.getDesktop().browse(new URI(tanke));
+                            System.exit(0);
+                        }else {
+                            System.out.println("fahrt möglich, Preis = " + fahrtkosten);
+                        }
+                    
+
+                    
+
+                
+                        
+
+    }
+
+}
